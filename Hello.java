0aa4913ae2976e7b7cdbf95b1f9a3b6da14a0d76
neelimagovindaraj <48053055+neelimagovindaraj@users.noreply.github.com>
2019-02-27 10:43:47 +0000

    adding hello.java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hi Its the first JAVA program");
+        int myFirstNumber = 12;
+        System.out.println("+as string concatinator gives 1212-->"+myFirstNumber+myFirstNumber);
+        System.out.println("myFirstNumbe it adds herer-->"+(myFirstNumber+myFirstNumber));
+    }
+
+}
